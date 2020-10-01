bd81011bb96706f9ac388558413aade2bb4d8e1c
gavinbrown025 <you@example.com>
2020-10-01 14:37:30 -0400

    fixed it and added Teams

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 //? import your packagess here
-import Person from "./modules/DataModule.js";
-import NavSystem from "./modules/TheNavModule.js";
+import Team from "./modules/DataModule.js";
+//import NavSystem from "./modules/TheNavModule.js";
 
 (() => {
     //? stub  * just a place for non component specific stuff
     console.log('loaded');
-    console.log(Person);
+    console.log(Team);
+
+    let userSection = document.querySelector(".user-section").children;
+    
+    userSection[1].textContent = Team["Gavin"].name;
+    userSection[2].textContent = Team["Gavin"].nickname;
+    userSection[3].textContent = Team["Gavin"].role;
 
 })();
